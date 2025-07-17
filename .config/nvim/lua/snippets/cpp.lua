d992063dda993248979ededd67251de5d3e56129
Luca Lobefaro <luca.lobefaro95@gmail.com>
2025-07-17 15:15:26 +0200

    Also cerr snippet

--- .config/nvim/lua/snippets/cpp.lua
+++ .config/nvim/lua/snippets/cpp.lua
@@ -3,5 +3,10 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-return {s("cout", {t("std::cout << "), i(1, "1"), t(" << std::endl;")})}
+return {
+    s({trig = "cout", dscr = "std::cout << ... << std::endl;"},
+      {t("std::cout << "), i(1, "1"), t(" << std::endl;")}),
+    s({trig = "cerr", dscr = "std::cerr << ... << std::endl;"},
+      {t("std::cerr << "), i(1, "error"), t(" << std::endl;")})
+}
 
